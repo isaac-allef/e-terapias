6fa0f9b2a4fa01dd6f912251b3d149c72960e2a8
Isaac Allef <isaac_allef@hotmail.com>
2021-02-24 18:24:23 -0300

    fix: import database settings

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import routes from './routes';
 import globalExceptionHandler from './middlewares/globalExceptionHandler';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
