3b4a08c004a146aa337d13a72600e6a68a60bcca
Isaac Allef <isaac_allef@hotmail.com>
2021-02-26 16:28:22 -0300

    creation of CreateFieldsService

---
+++ src/services/CreateFieldsService.ts
@@ -0,0 +1,54 @@
+import { getRepository } from 'typeorm';
+import Field from '../entities/Field';
+import FieldJournal from '../entities/FieldJournal';
+import AppError from '../errors/AppError';
+
+interface Field_request {
+    name: string;
+    type: string;
+    value: string | number | Date;
+}
+
+interface Request {
+    fieldJournal: FieldJournal;
+    fields: Field_request[];
+}
+
+class CreateFieldsService {
+    public async execute({ fieldJournal, fields }: Request): Promise<Field[]> {
+        const fieldRepository = getRepository(Field);
+
+        const fieldArray = fields.map(field => {
+            if (field.type === 'string') {
+                return fieldRepository.create({
+                    name: field.name,
+                    string_value: field.value as string,
+                    fieldJournal,
+                });
+            }
+            if (field.type === 'int') {
+                return fieldRepository.create({
+                    name: field.name,
+                    int_value: field.value as number,
+                    fieldJournal,
+                });
+            }
+
+            if (field.type === 'date') {
+                return fieldRepository.create({
+                    name: field.name,
+                    date_value: field.value as Date,
+                    fieldJournal,
+                });
+            }
+
+            throw new AppError('Field type not valide.', 500);
+        });
+
+        await fieldRepository.save(fieldArray);
+
+        return fieldArray;
+    }
+}
+
+export default CreateFieldsService;
