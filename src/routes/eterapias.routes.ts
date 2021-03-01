70a01770291603cfc17d6e8e52128414c7af539b
Isaac Allef <isaac_allef@hotmail.com>
2021-03-01 18:28:49 -0300

    creation of route to create, list and update eterapia

--- src/routes/eterapias.routes.ts
+++ src/routes/eterapias.routes.ts
@@ -1,17 +1,62 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import Eterapia from '../entities/Eterapia';
+import FieldJournalTemplate from '../entities/FieldJournalTemplate';
+import GetEterapiaByIdService from '../services/GetEterapiaByIdService';
+import GetFieldJournalTemplateByIdService from '../services/GetFieldJournalTemplateByIdService';
 
 const eterapiasRoute = Router();
 
-eterapiasRoute.post('/', (request, response) => {
-    return response.json({ message: 'Create' });
+eterapiasRoute.post('/', async (request, response) => {
+    const { name } = request.body;
+
+    const eterapiaRepository = getRepository(Eterapia);
+
+    const eterapia = eterapiaRepository.create({
+        name,
+    });
+
+    await eterapiaRepository.save(eterapia);
+
+    return response.json(eterapia);
 });
 
-eterapiasRoute.get('/', (request, response) => {
-    return response.json({ message: 'List' });
+eterapiasRoute.get('/', async (request, response) => {
+    const eterapiaRepository = getRepository(Eterapia);
+
+    const eterapias = await eterapiaRepository.find();
+
+    return response.json(eterapias);
 });
 
-eterapiasRoute.put('/', (request, response) => {
-    return response.json({ message: 'Update' });
+eterapiasRoute.put('/:id', async (request, response) => {
+    const { id } = request.params;
+
+    const { name, fieldJournalTemplateId } = request.body;
+
+    const eterapiaRepository = getRepository(Eterapia);
+
+    const getEterapiaById = new GetEterapiaByIdService();
+    const eterapia = await getEterapiaById.execute({
+        eterapiaId: id,
+        eterapiaRepository,
+    });
+
+    eterapia.name = name;
+
+    const fieldJournalTemplateRepository = getRepository(FieldJournalTemplate);
+
+    const getFieldJournalTemplateById = new GetFieldJournalTemplateByIdService();
+    const fieldJournalTemplate = await getFieldJournalTemplateById.execute({
+        fieldJournalTemplateId,
+        fieldJournalTemplateRepository,
+    });
+
+    eterapia.fieldJournalTemplate = fieldJournalTemplate;
+
+    eterapiaRepository.save(eterapia);
+
+    return response.json(eterapia);
 });
 
 eterapiasRoute.delete('/', (request, response) => {
