be4237a6abecb7fa2513e81c46c1fb4cd806a27a
Isaac Allef <isaac_allef@hotmail.com>
2021-03-02 13:16:27 -0300

    creation of AuthenticateModeratorService

---
+++ src/services/AuthenticateModeratorService.ts
@@ -0,0 +1,51 @@
+import { compare } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+import Moderator from '../entities/Moderator';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    moderator: Moderator;
+    token: string;
+}
+
+class AuthenticateModeratorService {
+    public async execute({ email, password }: Request): Promise<Response> {
+        const moderatorRepository = getRepository(Moderator);
+
+        const moderator = await moderatorRepository.findOne({
+            where: { email },
+        });
+
+        if (!moderator) {
+            throw new AppError('Incorrect email/password combination', 401);
+        }
+
+        const passwordMatched = await compare(password, moderator.password);
+
+        if (!passwordMatched) {
+            throw new AppError('Incorrect email/password combination', 401);
+        }
+
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({}, secret, {
+            subject: moderator.id,
+            expiresIn,
+        });
+
+        return {
+            moderator,
+            token,
+        };
+    }
+}
+
+export default AuthenticateModeratorService;
