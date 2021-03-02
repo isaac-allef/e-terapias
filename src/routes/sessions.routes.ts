5e524a80bcd7210afacf816befb6b25ada674c02
Isaac Allef <isaac_allef@hotmail.com>
2021-03-02 13:38:57 -0300

    modification of sessions routes to login from both, administrator and moderator

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import AuthenticateAdministratorService from '../services/AuthenticateAdministratorService';
+import AuthenticateModeratorService from '../services/AuthenticateModeratorService';
 
 const sessionsRouter = Router();
 
-sessionsRouter.post('/', async (request, response) => {
+sessionsRouter.post('/administrator', async (request, response) => {
     const { email, password } = request.body;
 
     const authenticateAdministrator = new AuthenticateAdministratorService();
@@ -16,6 +17,21 @@ sessionsRouter.post('/', async (request, response) => {
     delete administrator.password;
 
     return response.json({ administrator, token });
+});
+
+sessionsRouter.post('/moderator', async (request, response) => {
+    const { email, password } = request.body;
+
+    const authenticateModerator = new AuthenticateModeratorService();
+
+    const { moderator, token } = await authenticateModerator.execute({
+        email,
+        password,
+    });
+
+    delete moderator.password;
+
+    return response.json({ moderator, token });
 });
 
 export default sessionsRouter;
