413733d6a1561ed2e8073f41b72795a8ee1adb22
Isaac Allef <isaac_allef@hotmail.com>
2021-03-01 22:56:25 -0300

    creation of FieldsMatchWithFieldTemplatesService

---
+++ src/services/FieldsMatchWithFieldTemplatesService.ts
@@ -0,0 +1,40 @@
+interface Field_request {
+    name: string;
+    type: string;
+    value: string | number | Date | boolean;
+}
+
+interface FieldTemplates {
+    name: string;
+    type: string;
+}
+
+interface Request {
+    fields: Field_request[];
+    fieldTemplates: FieldTemplates[];
+}
+
+class FieldsMatchWithFieldTemplatesService {
+    public async execute({
+        fields,
+        fieldTemplates,
+    }: Request): Promise<boolean> {
+        if (fields.length !== fieldTemplates.length) {
+            return false;
+        }
+
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < fields.length; i++) {
+            if (fields[i].name !== fieldTemplates[i].name) {
+                return false;
+            }
+            if (fields[i].type !== fieldTemplates[i].type) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+export default FieldsMatchWithFieldTemplatesService;
