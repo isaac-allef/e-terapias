69efa8066e511af57e36ed1ff84112abf655c07e
Isaac Allef <isaac_allef@hotmail.com>
2021-03-13 09:55:58 -0300

    validate eterapia routes input

--- src/domains/eterapias/infra/http/routes/eterapias.routes.ts
+++ src/domains/eterapias/infra/http/routes/eterapias.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticatedAdministrator from '../../../../administrators/infra/http/middlewares/ensureAuthenticatedAdministrator';
 import EterapiaController from '../controllers/EterapiaController';
 
@@ -7,14 +8,42 @@ const eterapiaController = new EterapiaController();
 
 eterapiasRoute.use(ensureAuthenticatedAdministrator);
 
-eterapiasRoute.post('/', eterapiaController.create);
+eterapiasRoute.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+        },
+    }),
+    eterapiaController.create,
+);
 
 eterapiasRoute.get('/', eterapiaController.list);
 
 eterapiasRoute.get('/:id', eterapiaController.show);
 
-eterapiasRoute.put('/:id', eterapiaController.update);
+eterapiasRoute.put(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+        [Segments.BODY]: {
+            name: Joi.string(),
+            fieldJournalTemplateId: Joi.string().uuid(),
+        },
+    }),
+    eterapiaController.update,
+);
 
-eterapiasRoute.delete('/:id', eterapiaController.delete);
+eterapiasRoute.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    eterapiaController.delete,
+);
 
 export default eterapiasRoute;
