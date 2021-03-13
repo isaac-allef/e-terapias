67e36d6b3e40c3a0b548077a38c81a7077569c2e
Isaac Allef <isaac_allef@hotmail.com>
2021-03-13 10:45:48 -0300

    refactoring: delete function in RelationModeratorEterapiaController

--- src/domains/moderators/infra/http/controllers/RelationModeratorEterapiaController.ts
+++ src/domains/moderators/infra/http/controllers/RelationModeratorEterapiaController.ts
@@ -51,17 +51,19 @@ class RelationModeratorEterapiaController {
             throw new AppError('Moderator not found.');
         }
 
-        const eterapiaRepository = new EterapiaRepository();
-
-        const eterapia = await eterapiaRepository.findById(eterapiaId);
-
-        if (!eterapia) {
-            throw new AppError('Eterapia not found.');
-        }
-
-        const newEterapiasArray = moderator.eterapias.filter(ete => {
-            return ete.id !== eterapia.id;
+        let eterapiaRelateToThisModerator = false;
+        const newEterapiasArray = moderator.eterapias.filter(eterapia => {
+            if (eterapia.id === eterapiaId) {
+                eterapiaRelateToThisModerator = true;
+            }
+            return eterapia.id !== eterapiaId;
         });
+
+        if (!eterapiaRelateToThisModerator) {
+            throw new AppError(
+                'This eterapia does not relate to this moderator.',
+            );
+        }
 
         moderator.eterapias = newEterapiasArray;
 
