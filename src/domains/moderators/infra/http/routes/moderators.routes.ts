7393c03319b412cb3240f907c4719dce223360a7
Isaac Allef <isaac_allef@hotmail.com>
2021-03-13 11:59:54 -0300

    validate moderator routes input

--- src/domains/moderators/infra/http/routes/moderators.routes.ts
+++ src/domains/moderators/infra/http/routes/moderators.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticatedAdministrator from '../../../../administrators/infra/http/middlewares/ensureAuthenticatedAdministrator';
 import ModeratorController from '../controllers/ModeratorController';
 import RelationModeratorEterapiaController from '../controllers/RelationModeratorEterapiaController';
@@ -9,23 +10,63 @@ const relationModeratorEterapiaController = new RelationModeratorEterapiaControl
 
 moderatorsRoute.use(ensureAuthenticatedAdministrator);
 
-moderatorsRoute.post('/', moderatorController.create);
+moderatorsRoute.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+        },
+    }),
+    moderatorController.create,
+);
 
 moderatorsRoute.get('/', moderatorController.list);
 
 moderatorsRoute.get('/:id', moderatorController.show);
 
-moderatorsRoute.put('/:id', moderatorController.update);
+moderatorsRoute.put(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+        [Segments.BODY]: {
+            email: Joi.string().email(),
+        },
+    }),
+    moderatorController.update,
+);
 
-moderatorsRoute.delete('/:id', moderatorController.delete);
+moderatorsRoute.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    moderatorController.delete,
+);
 
 moderatorsRoute.patch(
     '/addEterapia',
+    celebrate({
+        [Segments.BODY]: {
+            moderatorId: Joi.string().uuid().required(),
+            eterapiaId: Joi.string().uuid().required(),
+        },
+    }),
     relationModeratorEterapiaController.create,
 );
 
 moderatorsRoute.patch(
     '/dropEterapia',
+    celebrate({
+        [Segments.BODY]: {
+            moderatorId: Joi.string().uuid().required(),
+            eterapiaId: Joi.string().uuid().required(),
+        },
+    }),
     relationModeratorEterapiaController.delete,
 );
 
