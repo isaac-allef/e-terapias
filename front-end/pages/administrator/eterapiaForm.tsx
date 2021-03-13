339829b9f5982bb260ba575a6134db9326b8dd7c
Isaac Allef <isaac_allef@hotmail.com>
2021-03-13 20:54:46 -0300

    creation of eterapia form page

---
+++ front-end/pages/administrator/eterapiaForm.tsx
@@ -0,0 +1,61 @@
+import { Field, Form, Formik } from "formik";
+import MyTitle from "../../components/MyTitle";
+import * as Yup from 'yup';
+import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Button } from "@chakra-ui/button";
+import { Divider } from "@chakra-ui/layout";
+
+export default function EterapiaForm() {
+    const SignupSchema = Yup.object().shape({
+        name: Yup.string().required('Required'),
+    });
+    
+    const initialValues = {
+        name: '',
+    }
+    
+    const functionSubmitForm = (values, actions) => {
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 1000)      
+        console.log(values);
+    }
+
+    return (
+      <>
+        <MyTitle>{'Create Eterapia'}</MyTitle>
+        <Formik
+        initialValues={initialValues}
+        validationSchema={SignupSchema}
+        onSubmit={functionSubmitForm}
+        >
+        {(props) => (
+            <Form>
+            <Field name="name">
+                {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.name && form.touched.name}>
+                        <FormLabel htmlFor="name">Name</FormLabel>
+                            <Input {...field} id="name" placeholder="name" />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                )}
+            </Field>
+
+            <Divider />
+
+            <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+                >
+                Submit
+            </Button>
+        </Form>
+        )}
+        </Formik>
+      </>
+  )
+}
