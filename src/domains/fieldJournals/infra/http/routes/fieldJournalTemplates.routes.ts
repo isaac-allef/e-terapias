b09af8cefe8ca0d6c359a7c05aebb6d953fde5bf
Isaac Allef <isaac_allef@hotmail.com>
2021-03-13 12:00:17 -0300

    validate field journal template routes input

--- src/domains/fieldJournals/infra/http/routes/fieldJournalTemplates.routes.ts
+++ src/domains/fieldJournals/infra/http/routes/fieldJournalTemplates.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticatedAdministrator from '../../../../administrators/infra/http/middlewares/ensureAuthenticatedAdministrator';
 import FieldJournalTemplateController from '../controllers/FieldJournalTemplateController';
 
@@ -7,7 +8,30 @@ const fieldJournalTemplateController = new FieldJournalTemplateController();
 
 fieldJournalTemplatesRoute.use(ensureAuthenticatedAdministrator);
 
-fieldJournalTemplatesRoute.post('/', fieldJournalTemplateController.create);
+fieldJournalTemplatesRoute.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            description: Joi.object()
+                .keys({
+                    title: Joi.string().required(),
+                    fieldTemplates: Joi.array()
+                        .items(
+                            Joi.object().keys({
+                                name: Joi.string().required(),
+                                type: Joi.string()
+                                    .required()
+                                    .valid('string', 'int', 'date', 'boolean'),
+                            }),
+                        )
+                        .required(),
+                })
+                .required(),
+        },
+    }),
+    fieldJournalTemplateController.create,
+);
 
 fieldJournalTemplatesRoute.get('/', fieldJournalTemplateController.list);
 
@@ -15,6 +39,11 @@ fieldJournalTemplatesRoute.get('/:id', fieldJournalTemplateController.show);
 
 fieldJournalTemplatesRoute.delete(
     '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
     fieldJournalTemplateController.delete,
 );
 
