53645701889e4c0659442cb4ce367c7fbcfeb5c4
Isaac Allef <isaac_allef@hotmail.com>
2021-03-29 18:14:05 -0300

    adding fieldJournalTemplate in CreateEterapiaService

--- back-end/src/domains/eterapias/services/CreateEterapiaService.ts
+++ back-end/src/domains/eterapias/services/CreateEterapiaService.ts
@@ -1,14 +1,36 @@
+import IFieldJournalTemplateRepository from '../../fieldJournals/repositories/IFieldJournalTemplateRepository';
 import ICreateEterapiaDTO from '../dtos/ICreateEterapiaDTO';
 import IEterapia from '../models/IEterapia';
 import IEterapiaRepository from '../repositories/IEterapiaRepository';
+import FindFieldJournalTemplateByIdService from './FindFieldJournalTemplateByIdService';
 
 class CreateEterapiaService {
-    constructor(private eterapiaRepository: IEterapiaRepository) {}
+    constructor(
+        private eterapiaRepository: IEterapiaRepository,
+        private fieldJournalTemplateRepository: IFieldJournalTemplateRepository,
+    ) {}
 
-    public async execute({ name }: ICreateEterapiaDTO): Promise<IEterapia> {
-        const eterapia = await this.eterapiaRepository.create({
+    public async execute({
+        name,
+        fieldJournalTemplateId,
+    }: ICreateEterapiaDTO): Promise<IEterapia> {
+        const eterapia = this.eterapiaRepository.createWithoutSave({
             name,
         });
+
+        if (fieldJournalTemplateId) {
+            const findFieldJournalTemplateByIdService = new FindFieldJournalTemplateByIdService(
+                this.fieldJournalTemplateRepository,
+            );
+
+            const fieldJournalTemplate = await findFieldJournalTemplateByIdService.execute(
+                { fieldJournalTemplateId },
+            );
+
+            eterapia.fieldJournalTemplate = fieldJournalTemplate;
+        }
+
+        await this.eterapiaRepository.save(eterapia);
 
         return eterapia;
     }
