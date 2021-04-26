fb19f779e8b7d256afca2156a0a6e16f2048736a
Isaac Allef <isaac_allef@hotmail.com>
2021-04-26 17:50:13 -0300

    alskjfad

--- back-end/src/app/http/routes/index.ts
+++ back-end/src/app/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import etherapyRouter from './etherapy.routes';
 import testRouter from './test.routes';
 
 const routes = Router();
 
 routes.use('/test', testRouter);
+routes.use('/etherapy', etherapyRouter);
 
 export default routes;
--- back-end/src/app/http/routes/test.routes.ts
+++ back-end/src/app/http/routes/test.routes.ts
@@ -1,39 +1,9 @@
 import { Router, Request, Response } from 'express';
-import makeCreateEtherapyService from '../../factories/makeCreateEtherapyService';
-import makeCreateModeratorService from '../../factories/makeCreateModeratorService';
 
-const testRouter = Router();
+const sessionsRouter = Router();
 
-testRouter.get('/', async (_request: Request, response: Response) => {
-    const etherapyName = 'viver é bom';
-    const etherapy = await makeCreateEtherapyService().execute(etherapyName);
-
-    if (!etherapy.id) {
-        return response.status(500).json({ message: 'etherapy id not found.' });
-    }
-
-    if (etherapy.name !== etherapyName) {
-        return response
-            .status(500)
-            .json({ message: 'incorrect etherapyName.' });
-    }
-
-    const moderatorName = 'viver é bom';
-    const moderator = await makeCreateModeratorService().execute(moderatorName);
-
-    if (!moderator.id) {
-        return response
-            .status(500)
-            .json({ message: 'moderator id not found.' });
-    }
-
-    if (moderator.name !== moderatorName) {
-        return response
-            .status(500)
-            .json({ message: 'incorrect moderatorName.' });
-    }
-
-    return response.json({ etherapy, moderator });
+sessionsRouter.post('/test', (_request: Request, response: Response) => {
+    return response;
 });
 
-export default testRouter;
+export default sessionsRouter;
