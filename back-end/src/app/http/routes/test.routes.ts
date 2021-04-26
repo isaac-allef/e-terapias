26a7cd8e13bad213d4b2830099934168e7fdb507
Isaac Allef <isaac_allef@hotmail.com>
2021-04-26 17:21:12 -0300

    create: test route

--- back-end/src/app/http/routes/test.routes.ts
+++ back-end/src/app/http/routes/test.routes.ts
@@ -1,9 +1,23 @@
 import { Router, Request, Response } from 'express';
+import makeCreateEtherapyService from '../../factories/makeCreateEtherapyService';
 
 const testRouter = Router();
 
-testRouter.get('/', (_request: Request, response: Response) => {
-    return response.json({ message: 'Ok' });
+testRouter.get('/', async (_request: Request, response: Response) => {
+    const etherapyName = 'viver é bom';
+    const etherapy = await makeCreateEtherapyService().execute(etherapyName);
+
+    if (!etherapy.id) {
+        return response.status(500).json({ message: 'etherapy id not found.' });
+    }
+
+    if (etherapy.name !== etherapyName) {
+        return response
+            .status(500)
+            .json({ message: 'incorrect etherapyName.' });
+    }
+
+    return response.json({ etherapy });
 });
 
 export default testRouter;
