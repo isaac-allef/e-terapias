68883ecbc88c4eac9647b4726f93546f463c4771
Isaac Allef <isaac_allef@hotmail.com>
2021-04-27 17:18:27 -0300

    feat: field journal verify match between template fields and fields

--- back-end/src/core/services/CreateFieldJournalService.ts
+++ back-end/src/core/services/CreateFieldJournalService.ts
@@ -1,6 +1,7 @@
 import Etherapy from '../entities/Etherapy';
 import FieldJournal, { field } from '../entities/FieldJournal';
 import Moderator from '../entities/Moderator';
+import { templateField } from '../entities/Template';
 import AppError from '../errors/AppError';
 import CreateFieldJournalRepository from '../protocols/db/repositories/CreateFieldJournalRepository';
 
@@ -25,6 +26,12 @@ class CreateFieldJournalService {
             throw new AppError('This therapy does not have a template.');
         }
 
+        const { templateFields } = etherapy.template;
+
+        if (!this.matchFieldsWithtemplateFields(templateFields, fields)) {
+            throw new AppError("This field journal doesn't match the template");
+        }
+
         const fieldJournal = await this.createFieldJournalRepository.create(
             name,
             fields,
@@ -33,6 +40,27 @@ class CreateFieldJournalService {
         );
 
         return fieldJournal;
+    }
+
+    private matchFieldsWithtemplateFields(
+        templateFields: templateField[],
+        fields: field[],
+    ): boolean {
+        const countTemplateFeilds = templateFields.length;
+        const countFields = fields.length;
+
+        if (countTemplateFeilds !== countFields) {
+            return false;
+        }
+
+        // eslint-disable-next-line
+        for (let i = 0; i < countTemplateFeilds; i++) {
+            if (templateFields[i].name !== fields[i].name) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
