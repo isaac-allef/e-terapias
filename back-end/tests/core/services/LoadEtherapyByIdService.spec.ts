cd1518abbbf1a600774ff9d5d99fc3a73f87a432
Isaac Allef <isaac_allef@hotmail.com>
2021-04-28 18:52:53 -0300

    test: LoadEtherapyByIdService should call with correct values

---
+++ back-end/tests/core/services/LoadEtherapyByIdService.spec.ts
@@ -0,0 +1,45 @@
+/* eslint-disable max-classes-per-file */
+import LoadEtherapyByIdService from '../../../src/core/services/LoadEtherapyByIdService';
+import LoadEtherapyByIdRepository from '../../../src/core/protocols/db/repositories/LoadEtherapyByIdRepository';
+import Etherapy from '../../../src/core/entities/Etherapy';
+
+const makeLoadEtherapyByIdRepository = (): LoadEtherapyByIdRepository => {
+    class LoadEtherapyByIdRepositoryStub implements LoadEtherapyByIdRepository {
+        async load(): Promise<Etherapy> {
+            const etherapy: Etherapy = {
+                id: 'randomId',
+                name: 'viver é bom',
+                fieldJournals: [],
+                moderators: [],
+                template: undefined,
+            };
+
+            return new Promise(resolve => resolve(etherapy));
+        }
+    }
+
+    return new LoadEtherapyByIdRepositoryStub();
+};
+
+interface SutTypes {
+    sut: LoadEtherapyByIdService;
+    loadEtherapyByIdRepository: LoadEtherapyByIdRepository;
+}
+
+const makeSut = (): SutTypes => {
+    const loadEtherapyByIdRepository = makeLoadEtherapyByIdRepository();
+    const sut = new LoadEtherapyByIdService(loadEtherapyByIdRepository);
+    return {
+        sut,
+        loadEtherapyByIdRepository,
+    };
+};
+
+describe('load Etherapy by id usecase', () => {
+    test('Should call with correct values', async () => {
+        const { sut } = makeSut();
+        const executeSpy = jest.spyOn(sut, 'execute');
+        await sut.execute('randomId');
+        expect(executeSpy).toHaveBeenCalledWith('randomId');
+    });
+});
