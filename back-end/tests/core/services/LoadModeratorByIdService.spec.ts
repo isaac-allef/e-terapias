d7cf3627f3e042c34bf36f1dd49e0a203f9ad983
Isaac Allef <isaac_allef@hotmail.com>
2021-04-28 19:03:16 -0300

    test: LoadModeratorByIdService should call with correct values

---
+++ back-end/tests/core/services/LoadModeratorByIdService.spec.ts
@@ -0,0 +1,48 @@
+/* eslint-disable max-classes-per-file */
+import LoadModeratorByIdService from '../../../src/core/services/LoadModeratorByIdService';
+import LoadModeratorByIdRepository from '../../../src/core/protocols/db/repositories/LoadModeratorByIdRepository';
+import Moderator from '../../../src/core/entities/Moderator';
+
+const makeLoadModeratorByIdRepository = (): LoadModeratorByIdRepository => {
+    class LoadModeratorByIdRepositoryStub
+        implements LoadModeratorByIdRepository {
+        async load(): Promise<Moderator> {
+            const moderator: Moderator = {
+                id: 'randomId',
+                email: 'fulano@email.com',
+                name: 'fulano',
+                etherapies: [],
+                fieldJournals: [],
+                password: '1234',
+                token: 'randomToken',
+            };
+
+            return new Promise(resolve => resolve(moderator));
+        }
+    }
+
+    return new LoadModeratorByIdRepositoryStub();
+};
+
+interface SutTypes {
+    sut: LoadModeratorByIdService;
+    loadModeratorByIdRepository: LoadModeratorByIdRepository;
+}
+
+const makeSut = (): SutTypes => {
+    const loadModeratorByIdRepository = makeLoadModeratorByIdRepository();
+    const sut = new LoadModeratorByIdService(loadModeratorByIdRepository);
+    return {
+        sut,
+        loadModeratorByIdRepository,
+    };
+};
+
+describe('load Moderator by id usecase', () => {
+    test('Should call with correct values', async () => {
+        const { sut } = makeSut();
+        const executeSpy = jest.spyOn(sut, 'execute');
+        await sut.execute('randomId');
+        expect(executeSpy).toHaveBeenCalledWith('randomId');
+    });
+});
