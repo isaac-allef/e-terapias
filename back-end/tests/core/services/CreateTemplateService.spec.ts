00344ea822b2ecccc8f711f20f52b9e0ee3a7c84
Isaac Allef <isaac_allef@hotmail.com>
2021-04-28 18:36:26 -0300

    test: CreateTemplateService should call with correct values

---
+++ back-end/tests/core/services/CreateTemplateService.spec.ts
@@ -0,0 +1,71 @@
+/* eslint-disable max-classes-per-file */
+import CreateTemplateService from '../../../src/core/services/CreateTemplateService';
+import CreateTemplateRepository from '../../../src/core/protocols/db/repositories/CreateTemplateRepository';
+import Template from '../../../src/core/entities/Template';
+
+const makeCreateTemplateRepository = (): CreateTemplateRepository => {
+    class CreateTemplateRepositoryStub implements CreateTemplateRepository {
+        async create(): Promise<Template> {
+            const template: Template = {
+                id: 'randomId',
+                name: 'diário das eterapias de promoção ao bem-estar',
+                etherapies: [],
+                templateFields: [
+                    { name: 'Qual o seu nome?' },
+                    { name: 'Quanto é 2 + 2?' },
+                    {
+                        name: 'Informe sua data de nascimento',
+                    },
+                    { name: 'Voçê é estudante?' },
+                ],
+            };
+
+            return new Promise(resolve => resolve(template));
+        }
+    }
+
+    return new CreateTemplateRepositoryStub();
+};
+
+interface SutTypes {
+    sut: CreateTemplateService;
+    createTemplateRepository: CreateTemplateRepository;
+}
+
+const makeSut = (): SutTypes => {
+    const createTemplateRepository = makeCreateTemplateRepository();
+    const sut = new CreateTemplateService(createTemplateRepository);
+    return {
+        sut,
+        createTemplateRepository,
+    };
+};
+
+describe('Create Template usecase', () => {
+    test('Should call with correct values', async () => {
+        const { sut } = makeSut();
+        const executeSpy = jest.spyOn(sut, 'execute');
+        await sut.execute({
+            name: 'diário das eterapias de promoção ao bem-estar',
+            templateFields: [
+                { name: 'Qual o seu nome?' },
+                { name: 'Quanto é 2 + 2?' },
+                {
+                    name: 'Informe sua data de nascimento',
+                },
+                { name: 'Voçê é estudante?' },
+            ],
+        });
+        expect(executeSpy).toHaveBeenCalledWith({
+            name: 'diário das eterapias de promoção ao bem-estar',
+            templateFields: [
+                { name: 'Qual o seu nome?' },
+                { name: 'Quanto é 2 + 2?' },
+                {
+                    name: 'Informe sua data de nascimento',
+                },
+                { name: 'Voçê é estudante?' },
+            ],
+        });
+    });
+});
