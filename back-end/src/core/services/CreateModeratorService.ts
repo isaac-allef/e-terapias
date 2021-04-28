543f91e159fa1a150fc98606d939a0ce1aa6e9af
Isaac Allef <isaac_allef@hotmail.com>
2021-04-28 15:49:35 -0300

    feat: CreateModeratorService should call with correct values

--- back-end/src/core/services/CreateModeratorService.ts
+++ back-end/src/core/services/CreateModeratorService.ts
@@ -1,10 +1,15 @@
 import Moderator from '../entities/Moderator';
 import CreateModeratorRepository from '../protocols/db/repositories/CreateModeratorRepository';
 
+export type params = {
+    email: string;
+    name: string;
+};
+
 class CreateModeratorService {
     constructor(private createModeratorRepository: CreateModeratorRepository) {}
 
-    public async execute(email: string, name: string): Promise<Moderator> {
+    public async execute({ email, name }: params): Promise<Moderator> {
         const moderator = await this.createModeratorRepository.create(
             email,
             name,
