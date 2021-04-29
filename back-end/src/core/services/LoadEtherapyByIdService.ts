062a4f6def7968aa76ca4bdcce11eaa4a385351d
Isaac Allef <isaac_allef@hotmail.com>
2021-04-29 20:10:09 -0300

    feat: LoadEtherapyByIdService should throw AppError if LoadEtherapyByIdRepository return undefined

--- back-end/src/core/services/LoadEtherapyByIdService.ts
+++ back-end/src/core/services/LoadEtherapyByIdService.ts
@@ -1,4 +1,5 @@
 import Etherapy from '../entities/Etherapy';
+import AppError from '../errors/AppError';
 import LoadEtherapyByIdRepository from '../protocols/db/repositories/LoadEtherapyByIdRepository';
 
 class LoadEtherapyByIdService {
@@ -8,6 +9,10 @@ class LoadEtherapyByIdService {
 
     public async execute(id: string): Promise<Etherapy> {
         const etherapy = await this.loadEtherapyByIdRepository.load(id);
+
+        if (!etherapy) {
+            throw new AppError('Moderator not found.');
+        }
 
         return etherapy;
     }
