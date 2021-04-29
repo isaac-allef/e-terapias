d29a7fd8e45fc9a64728caafef07965195a4d95e
Isaac Allef <isaac_allef@hotmail.com>
2021-04-29 10:47:04 -0300

    test: create many etherapies at once

--- back-end/tests/core/services/CreateEtherapiesService.spec.ts
+++ back-end/tests/core/services/CreateEtherapiesService.spec.ts
@@ -1,16 +1,16 @@
 /* eslint-disable max-classes-per-file */
-import CreateEtherapyService from '../../../src/core/services/CreateEtherapyService';
+import CreateEtherapiesService from '../../../src/core/services/CreateEtherapiesService';
 import CreateEtherapyRepository from '../../../src/core/protocols/db/repositories/CreateEtherapyRepository';
 import { CreateEtherapyRepositoryStub } from '../mocks/mockEtherapy';
 
 interface SutTypes {
-    sut: CreateEtherapyService;
+    sut: CreateEtherapiesService;
     createEtherapyRepository: CreateEtherapyRepository;
 }
 
 const makeSut = (): SutTypes => {
     const createEtherapyRepository = new CreateEtherapyRepositoryStub();
-    const sut = new CreateEtherapyService(createEtherapyRepository);
+    const sut = new CreateEtherapiesService(createEtherapyRepository);
     return {
         sut,
         createEtherapyRepository,
@@ -21,16 +21,18 @@ describe('Create Etherapy usecase', () => {
     test('Should call with correct values', async () => {
         const { sut } = makeSut();
         const executeSpy = jest.spyOn(sut, 'execute');
-        await sut.execute({ name: 'viver é bom' });
-        expect(executeSpy).toHaveBeenCalledWith({
-            name: 'viver é bom',
-        });
+        await sut.execute([{ name: 'viver é bom' }, { name: 'não desista' }]);
+        expect(executeSpy).toHaveBeenCalledWith([
+            { name: 'viver é bom' },
+            { name: 'não desista' },
+        ]);
     });
 
     test('Should call CreateEtherapyRepository with correct values', async () => {
         const { sut, createEtherapyRepository } = makeSut();
         const createSpy = jest.spyOn(createEtherapyRepository, 'create');
-        await sut.execute({ name: 'viver é bom' });
+        await sut.execute([{ name: 'viver é bom' }, { name: 'não desista' }]);
         expect(createSpy).toHaveBeenCalledWith({ name: 'viver é bom' });
+        expect(createSpy).toHaveBeenCalledWith({ name: 'não desista' });
     });
 });
