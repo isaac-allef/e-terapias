0f0715935484e6be1038386011bf9840da240c19
Isaac Allef <isaac_allef@hotmail.com>
2021-04-30 17:48:48 -0300

    feat: LinkTemplateToEtherapiesServce remove unnecessary validations

--- back-end/src/core/services/LinkTemplateToEtherapiesService.ts
+++ back-end/src/core/services/LinkTemplateToEtherapiesService.ts
@@ -1,4 +1,3 @@
-import AppError from '../errors/AppError';
 import LinkTemplateToEtherapiesRepository from '../protocols/db/repositories/LinkTemplateToEtherapiesRepository';
 import LoadEtherapyByIdRepository from '../protocols/db/repositories/LoadEtherapyByIdRepository';
 import LoadTemplateByIdRepository from '../protocols/db/repositories/LoadTemplateByIdRepository';
@@ -16,17 +15,9 @@ class LinkTemplateToEtherapiesService {
     ): Promise<boolean> {
         const template = await this.loadTemplateByIdRepository.load(templateId);
 
-        if (!template) {
-            throw new AppError('Template not found.');
-        }
-
         const etherapies = await Promise.all(
             etherapiesIds.map(async id => {
                 const etherapy = await this.loadEtherapyByIdRepository.load(id);
-
-                if (!etherapy) {
-                    throw new AppError('Etherapy not found.');
-                }
 
                 return etherapy;
             }),
