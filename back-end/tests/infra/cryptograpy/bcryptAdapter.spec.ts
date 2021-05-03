09e76d9737f8752f49d54a43ed652d8b92345b2e
Isaac Allef <isaac_allef@hotmail.com>
2021-05-03 15:08:15 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- back-end/tests/infra/cryptograpy/bcryptAdapter.spec.ts
+++ back-end/tests/infra/cryptograpy/bcryptAdapter.spec.ts
@@ -45,4 +45,10 @@ describe('Bcrypt Adapter', () => {
         await sut.compare('any_value', 'any_hash');
         expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
     });
+
+    test('Should return true when compare succeeds', async () => {
+        const sut = makeSut();
+        const isvalid = await sut.compare('any_value', 'any_hash');
+        expect(isvalid).toBe(true);
+    });
 });
