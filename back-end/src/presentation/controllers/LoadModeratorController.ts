4b92de58524e5cdafaa67e683720c2a9a7b7e597
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 16:36:00 -0300

    feat: create LoadModeratorController

---
+++ back-end/src/presentation/controllers/LoadModeratorController.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+import LoadModeratorByIdService from '../../core/services/LoadModeratorByIdService';
+import { MissingParamError } from '../erros/missingParamError';
+import { badRequest, ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class LoadModeratorController implements Controller {
+    constructor(
+        private readonly loadModeratorByIdService: LoadModeratorByIdService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { id } = httpRequest.params;
+
+            if (!id) {
+                return badRequest(new MissingParamError('id'));
+            }
+
+            const moderator = await this.loadModeratorByIdService.execute(id);
+            return ok(moderator);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
