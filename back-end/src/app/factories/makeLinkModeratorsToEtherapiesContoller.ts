248c6441062f360c7164faf4ecb9d5d4b9d5bb84
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 17:53:08 -0300

    feat: create LinkModeratorsToEtherapiesController controller

---
+++ back-end/src/app/factories/makeLinkModeratorsToEtherapiesContoller.ts
@@ -0,0 +1,21 @@
+import LinkModeratorsToEtherapiesService from '../../core/services/LinkModeratorsToEtherapiesService';
+import ModeratorTypeormRepository from '../../infra/db/typeorm/repositories/ModeratorTypeormRepository';
+import EtherapyTypeormRepository from '../../infra/db/typeorm/repositories/EtherapyTypeormRepository';
+import { LinkModeratorsToEtherapiesController } from '../../presentation/controllers/LinkModeratorsToEtherapiesController';
+import { Controller } from '../../presentation/protocols/controller';
+
+const makeLinkModeratorsToEtherapiesContoller = (): Controller => {
+    const linkModeratorsToEtherapiesRepository = new EtherapyTypeormRepository();
+    const loadModeratorRepository = new ModeratorTypeormRepository();
+    const loadEtherapyRepository = new EtherapyTypeormRepository();
+    const linkModeratorsToEtherapiesService = new LinkModeratorsToEtherapiesService(
+        linkModeratorsToEtherapiesRepository,
+        loadModeratorRepository,
+        loadEtherapyRepository,
+    );
+    return new LinkModeratorsToEtherapiesController(
+        linkModeratorsToEtherapiesService,
+    );
+};
+
+export default makeLinkModeratorsToEtherapiesContoller;
