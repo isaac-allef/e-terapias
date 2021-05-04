5606aeadeeb6289b2d453fbdfd11b58354036067
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 18:40:57 -0300

    feat: add link template to etherapies route

--- back-end/src/app/http/routes/links.routes.ts
+++ back-end/src/app/http/routes/links.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import adapterRouter from '../adapters/expressRouter';
 import makeLinkModeratorsToEtherapiesContoller from '../../factories/makeLinkModeratorsToEtherapiesContoller';
+import makeLinkTemplateToEtherapiesContoller from '../../factories/makeLinkTemplateToEtherapiesContoller';
 
 const linksRouter = Router();
 
 linksRouter.post(
     '/moderators/etherapies',
     adapterRouter(makeLinkModeratorsToEtherapiesContoller()),
+);
+
+linksRouter.post(
+    '/template/etherapies',
+    adapterRouter(makeLinkTemplateToEtherapiesContoller()),
 );
 
 export default linksRouter;
