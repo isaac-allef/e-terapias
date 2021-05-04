2e7f68d21ae53c6aa5a9fd01aa71729a769193d5
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 20:50:39 -0300

    feat: create CreateFieldJournalController

---
+++ back-end/src/presentation/controllers/CreateFieldJournalController.ts
@@ -0,0 +1,44 @@
+/* eslint-disable import/prefer-default-export */
+import CreateFieldJournalService from '../../core/services/CreateFieldJournalService';
+import { MissingParamError } from '../erros/missingParamError';
+import { badRequest, ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class CreateFieldJournalController implements Controller {
+    constructor(
+        private readonly createFieldJournalService: CreateFieldJournalService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { name, fields, moderatorId, etherapyId } = httpRequest.body;
+
+            if (!name) {
+                return badRequest(new MissingParamError('name'));
+            }
+
+            if (!fields) {
+                return badRequest(new MissingParamError('fields'));
+            }
+
+            if (!moderatorId) {
+                return badRequest(new MissingParamError('moderatorId'));
+            }
+
+            if (!etherapyId) {
+                return badRequest(new MissingParamError('etherapyId'));
+            }
+
+            const FieldJournal = await this.createFieldJournalService.execute({
+                name,
+                fields,
+                moderatorId,
+                etherapyId,
+            });
+            return ok(FieldJournal);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
