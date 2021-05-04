c22f1183c7d20783c7dae6d69c24a5575de9d351
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 16:36:30 -0300

    feat: add load moderator route

--- back-end/src/app/http/routes/moderators.routes.ts
+++ back-end/src/app/http/routes/moderators.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import adapterRouter from '../adapters/expressRouter';
 import makeCreateModeratorsContoller from '../../factories/makeCreateModeratorsContoller';
+import makeLoadModeratorContoller from '../../factories/makeLoadModeratorContoller';
 
 const moderatorsRouter = Router();
 
 moderatorsRouter.post('/', adapterRouter(makeCreateModeratorsContoller()));
+moderatorsRouter.get('/:id', adapterRouter(makeLoadModeratorContoller()));
 
 export default moderatorsRouter;
