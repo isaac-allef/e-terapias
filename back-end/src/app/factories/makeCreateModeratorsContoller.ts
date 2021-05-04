ead0e76de00f192852d1e6eed9c99d6cdbd5d99f
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 11:27:47 -0300

    feat: create CreateModeratorsController factory

---
+++ back-end/src/app/factories/makeCreateModeratorsContoller.ts
@@ -0,0 +1,17 @@
+import CreateModeratorsService from '../../core/services/CreateModeratorsService';
+import BcryptAdapter from '../../infra/cryptography/bcryptAdapter';
+import ModeratorTypeormRepository from '../../infra/db/typeorm/repositories/ModeratorTypeormRepository';
+import { CreateModeratorsController } from '../../presentation/controllers/CreateModeratorsController';
+import { Controller } from '../../presentation/protocols/controller';
+
+const makeCreateModeratorsContoller = (): Controller => {
+    const createModeratorsRepository = new ModeratorTypeormRepository();
+    const hashGenerater = new BcryptAdapter(12);
+    const createModeratorsService = new CreateModeratorsService(
+        hashGenerater,
+        createModeratorsRepository,
+    );
+    return new CreateModeratorsController(createModeratorsService);
+};
+
+export default makeCreateModeratorsContoller;
