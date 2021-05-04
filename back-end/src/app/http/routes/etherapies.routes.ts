a275d3f41eab3ec810e55c84bacb02c8ab98f7e7
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 16:26:18 -0300

    feat: add load etherapy route

--- back-end/src/app/http/routes/etherapies.routes.ts
+++ back-end/src/app/http/routes/etherapies.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import adapterRouter from '../adapters/expressRouter';
 import makeCreateEtherapiesContoller from '../../factories/makeCreateEtherapiesContoller';
+import makeLoadEtherapyContoller from '../../factories/makeLoadEtherapyContoller';
 
 const etherapiesRouter = Router();
 
 etherapiesRouter.post('/', adapterRouter(makeCreateEtherapiesContoller()));
+etherapiesRouter.get('/:id', adapterRouter(makeLoadEtherapyContoller()));
 
 export default etherapiesRouter;
