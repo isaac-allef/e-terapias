903e5deda1a884d135139b0465f04c04b61087b7
Isaac Allef <isaac_allef@hotmail.com>
2021-05-04 16:36:10 -0300

    feat: create LoadModeratorController factory

---
+++ back-end/src/app/factories/makeLoadModeratorContoller.ts
@@ -0,0 +1,14 @@
+import LoadModeratorByIdService from '../../core/services/LoadModeratorByIdService';
+import ModeratorTypeormRepository from '../../infra/db/typeorm/repositories/ModeratorTypeormRepository';
+import { LoadModeratorController } from '../../presentation/controllers/LoadModeratorController';
+import { Controller } from '../../presentation/protocols/controller';
+
+const makeLoadModeratorContoller = (): Controller => {
+    const loadModeratorRepository = new ModeratorTypeormRepository();
+    const loadModeratorByIdService = new LoadModeratorByIdService(
+        loadModeratorRepository,
+    );
+    return new LoadModeratorController(loadModeratorByIdService);
+};
+
+export default makeLoadModeratorContoller;
