24dcbdcf87c079de1f297bb171fc2b2c353a2e07
Isaac Allef <isaac_allef@hotmail.com>
2021-05-05 19:26:06 -0300

    feat: create SearchEtherapiesController factory

---
+++ back-end/src/app/factories/makeSearchEtherapiesContoller.ts
@@ -0,0 +1,14 @@
+import SearchEtherapiesService from '../../core/services/SearchEtherapiesService';
+import EtherapyTypeormRepository from '../../infra/db/typeorm/repositories/EtherapyTypeormRepository';
+import { SearchEtherapiesController } from '../../presentation/controllers/SearchEtherapiesController';
+import { Controller } from '../../presentation/protocols/controller';
+
+const makeSearchEtherapiesContoller = (): Controller => {
+    const searchEtherapiesRepository = new EtherapyTypeormRepository();
+    const searchEtherapiesService = new SearchEtherapiesService(
+        searchEtherapiesRepository,
+    );
+    return new SearchEtherapiesController(searchEtherapiesService);
+};
+
+export default makeSearchEtherapiesContoller;
