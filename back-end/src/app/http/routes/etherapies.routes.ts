6a9333dc1155110529593666a9d41d13e30254a8
Isaac Allef <isaac_allef@hotmail.com>
2021-05-05 19:26:32 -0300

    feat: add search etherapies route

--- back-end/src/app/http/routes/etherapies.routes.ts
+++ back-end/src/app/http/routes/etherapies.routes.ts
@@ -3,10 +3,15 @@ import adapterRouter from '../adapters/expressRouter';
 import makeCreateEtherapiesContoller from '../../factories/makeCreateEtherapiesContoller';
 import makeLoadEtherapyContoller from '../../factories/makeLoadEtherapyContoller';
 import makeLoadAllEtherapiesContoller from '../../factories/makeLoadAllEtherapiesContoller';
+import makeSearchEtherapiesContoller from '../../factories/makeSearchEtherapiesContoller';
 
 const etherapiesRouter = Router();
 
 etherapiesRouter.post('/', adapterRouter(makeCreateEtherapiesContoller()));
+etherapiesRouter.get(
+    '/search/:keyword',
+    adapterRouter(makeSearchEtherapiesContoller()),
+);
 etherapiesRouter.get('/:id', adapterRouter(makeLoadEtherapyContoller()));
 etherapiesRouter.get('/', adapterRouter(makeLoadAllEtherapiesContoller()));
 
