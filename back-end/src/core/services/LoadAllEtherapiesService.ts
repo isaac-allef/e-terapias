5cbecd574fb934a02642f1c883b5d44476b90bdc
Isaac Allef <isaac_allef@hotmail.com>
2021-05-05 16:58:15 -0300

    feat: create LoadAllEtherapiesServce

---
+++ back-end/src/core/services/LoadAllEtherapiesService.ts
@@ -0,0 +1,26 @@
+import Etherapy from '../entities/Etherapy';
+import LoadAllEtherapiesRepository, {
+    params,
+} from '../protocols/db/repositories/LoadAllEtherapiesRepository';
+
+class LoadAllEtherapiesService {
+    constructor(
+        private loadAllEtherapiesRepository: LoadAllEtherapiesRepository,
+    ) {}
+
+    public async execute({
+        sort = 'updated_at',
+        direction = 'asc',
+        per_page = 10,
+        page = 1,
+    }: params): Promise<Etherapy[]> {
+        return this.loadAllEtherapiesRepository.loadAll({
+            sort,
+            direction,
+            per_page,
+            page,
+        });
+    }
+}
+
+export default LoadAllEtherapiesService;
