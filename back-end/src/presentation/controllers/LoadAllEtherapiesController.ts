0fd42be7a318b23829d545b3bbf1d842de9cafd4
Isaac Allef <isaac_allef@hotmail.com>
2021-05-05 18:03:02 -0300

    feat: create LoadAllEtherapiesController

---
+++ back-end/src/presentation/controllers/LoadAllEtherapiesController.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+import LoadAllEtherapiesService from '../../core/services/LoadAllEtherapiesService';
+import { ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class LoadAllEtherapiesController implements Controller {
+    constructor(
+        private readonly loadAllEtherapiesService: LoadAllEtherapiesService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { sort, direction, per_page, page } = httpRequest.query;
+
+            const etherapy = await this.loadAllEtherapiesService.execute({
+                sort,
+                direction,
+                per_page,
+                page,
+            });
+            return ok(etherapy);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
