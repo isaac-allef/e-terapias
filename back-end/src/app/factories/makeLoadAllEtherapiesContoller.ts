8d858a41f789b0019d043d207ed4784c11d41c21
Isaac Allef <isaac_allef@hotmail.com>
2021-05-05 18:03:13 -0300

    feat: create LoadAllEtherapiesController factory

---
+++ back-end/src/app/factories/makeLoadAllEtherapiesContoller.ts
@@ -0,0 +1,14 @@
+import LoadAllEtherapiesService from '../../core/services/LoadAllEtherapiesService';
+import EtherapyTypeormRepository from '../../infra/db/typeorm/repositories/EtherapyTypeormRepository';
+import { LoadAllEtherapiesController } from '../../presentation/controllers/LoadAllEtherapiesController';
+import { Controller } from '../../presentation/protocols/controller';
+
+const makeLoadAllEtherapiesContoller = (): Controller => {
+    const loadAllEtherapiesRepository = new EtherapyTypeormRepository();
+    const loadAllEtherapiesService = new LoadAllEtherapiesService(
+        loadAllEtherapiesRepository,
+    );
+    return new LoadAllEtherapiesController(loadAllEtherapiesService);
+};
+
+export default makeLoadAllEtherapiesContoller;
