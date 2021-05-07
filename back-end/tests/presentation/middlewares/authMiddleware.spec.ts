cab77e12fb0f610e3dba46b8f42d61abc9441f02
Isaac Allef <isaac_allef@hotmail.com>
2021-05-07 15:10:37 -0300

    test: ensure AuthMiddleware should call LoadUserByToken with correct accessToken

--- back-end/tests/presentation/middlewares/authMiddleware.spec.ts
+++ back-end/tests/presentation/middlewares/authMiddleware.spec.ts
@@ -1,11 +1,48 @@
 import { forbidden } from '../../../src/presentation/helpers/httpHelder';
 import { AccessDeniedError } from '../../../src/presentation/erros/AccessDeniedError';
 import { AuthMiddleware } from '../../../src/presentation/middlewares/authMiddleware';
+import LoadUserByTokenService from '../../../src/core/services/LoadUserByTokenService';
+import { TokenDecodederStub } from '../../core/mocks/mockToken';
+import { LoadUserByTokenRepositoryStub } from '../../core/mocks/mockUser';
+
+const makeLoadUserByTokenService = (): LoadUserByTokenService => {
+    const tokenDecodederStub = new TokenDecodederStub();
+    const loadUserByTokenRepositoryStub = new LoadUserByTokenRepositoryStub();
+    return new LoadUserByTokenService(
+        tokenDecodederStub,
+        loadUserByTokenRepositoryStub,
+    );
+};
+
+interface SutTypes {
+    sut: AuthMiddleware;
+    loadUserByTokenServiceStub: LoadUserByTokenService;
+}
+
+const makeSut = (): SutTypes => {
+    const loadUserByTokenServiceStub = makeLoadUserByTokenService();
+    const sut = new AuthMiddleware(loadUserByTokenServiceStub);
+    return {
+        sut,
+        loadUserByTokenServiceStub,
+    };
+};
 
 describe('Auth Middleware', () => {
     test('Should return 403 if no x-access-token exists in headers', async () => {
-        const sut = new AuthMiddleware();
+        const { sut } = makeSut();
         const httpResponse = await sut.handle({});
         expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+    });
+
+    test('Should call LoadUserByToken with correct accessToken', async () => {
+        const { sut, loadUserByTokenServiceStub } = makeSut();
+        const executeSpy = jest.spyOn(loadUserByTokenServiceStub, 'execute');
+        await sut.handle({
+            headers: {
+                'x-access-token': 'any_token',
+            },
+        });
+        expect(executeSpy).toHaveBeenCalledWith('any_token');
     });
 });
