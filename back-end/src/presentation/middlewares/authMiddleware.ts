911b55c8ecbdd37f8415ded1863d74c289f1585f
Isaac Allef <isaac_allef@hotmail.com>
2021-05-07 15:10:25 -0300

    feat: ensure AuthMiddleware should call LoadUserByToken with correct accessToken

--- back-end/src/presentation/middlewares/authMiddleware.ts
+++ back-end/src/presentation/middlewares/authMiddleware.ts
@@ -1,11 +1,20 @@
 /* eslint-disable import/prefer-default-export */
+import LoadUserByTokenService from '../../core/services/LoadUserByTokenService';
 import { AccessDeniedError } from '../erros/AccessDeniedError';
 import { forbidden } from '../helpers/httpHelder';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { Middelware } from '../protocols/middleware';
 
 export class AuthMiddleware implements Middelware {
-    async handle(_httpRequest: HttpRequest): Promise<HttpResponse> {
+    constructor(
+        private readonly loadUserByTokenService: LoadUserByTokenService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const accessToken = httpRequest.headers?.['x-access-token'];
+        if (accessToken) {
+            await this.loadUserByTokenService.execute(accessToken);
+        }
         const error = forbidden(new AccessDeniedError());
         return new Promise(resolve => resolve(error));
     }
