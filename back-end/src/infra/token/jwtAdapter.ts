b893741dc9bcc058bc5653c025e2dc52347cd126
Isaac Allef <isaac_allef@hotmail.com>
2021-05-07 20:17:00 -0300

    feat: ensure JwtAdapter call verify with correct values

--- back-end/src/infra/token/jwtAdapter.ts
+++ back-end/src/infra/token/jwtAdapter.ts
@@ -1,11 +1,17 @@
 import jwt from 'jsonwebtoken';
+import TokenDecodeder from '../../core/protocols/Token/TokenDecodeder';
 import TokenGenerater from '../../core/protocols/Token/TokenGenerater';
 
-export default class JwtAdapter implements TokenGenerater {
+export default class JwtAdapter implements TokenGenerater, TokenDecodeder {
     constructor(private readonly secret: string) {}
 
     async generate(value: string): Promise<string> {
         const accessToken = await jwt.sign({ id: value }, this.secret);
         return accessToken;
+    }
+
+    async decode(value: string): Promise<string> {
+        await jwt.verify(value, this.secret);
+        return '';
     }
 }
