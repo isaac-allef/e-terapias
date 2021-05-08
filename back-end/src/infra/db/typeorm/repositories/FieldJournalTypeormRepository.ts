080f8ad131ee47b3c8432ad4538f6e06690306e6
Isaac Allef <isaac_allef@hotmail.com>
2021-05-08 20:38:21 -0300

    feat: implement LoadFieldJournalsPerModeratorRepository

--- back-end/src/infra/db/typeorm/repositories/FieldJournalTypeormRepository.ts
+++ back-end/src/infra/db/typeorm/repositories/FieldJournalTypeormRepository.ts
@@ -2,21 +2,27 @@
 import { EntityRepository, getRepository, Repository } from 'typeorm';
 import FieldJournal from '../../../../core/entities/FieldJournal';
 import CreateFieldJournalRepository, {
-    params,
+    params as createParams,
 } from '../../../../core/protocols/db/repositories/CreateFieldJournalRepository';
+import LoadAllFieldJournalsPerModeratorRepository, {
+    params as loadAllPerModeratorParams,
+} from '../../../../core/protocols/db/repositories/LoadAllFieldJournalsPerModeratorRepository';
 import LoadFieldJournalByIdRepository from '../../../../core/protocols/db/repositories/LoadFieldJournalByIdRepository';
 import FieldJournalTypeorm from '../entities/FieldJournalTypeorm';
 
 @EntityRepository()
 class FieldJournalTypeormRepository
-    implements CreateFieldJournalRepository, LoadFieldJournalByIdRepository {
+    implements
+        CreateFieldJournalRepository,
+        LoadFieldJournalByIdRepository,
+        LoadAllFieldJournalsPerModeratorRepository {
     private ormRepository: Repository<FieldJournalTypeorm>;
 
     constructor() {
         this.ormRepository = getRepository(FieldJournalTypeorm);
     }
 
-    public async create(data: params): Promise<FieldJournal> {
+    public async create(data: createParams): Promise<FieldJournal> {
         try {
             const { name, fields, moderator, etherapy } = data;
             const fieldJournal = this.ormRepository.create({
@@ -48,6 +54,28 @@ class FieldJournalTypeormRepository
             return fieldJournal;
         } catch {
             throw new Error('Load fieldJournal error');
+        }
+    }
+
+    async loadAllPerModerator({
+        moderatorId,
+        sort,
+        direction,
+        per_page,
+        page,
+    }: loadAllPerModeratorParams): Promise<FieldJournal[]> {
+        try {
+            const etherapies = await this.ormRepository.find({
+                where: { moderator: { id: moderatorId } },
+                order: { [sort]: direction.toUpperCase() },
+                take: per_page,
+                skip: (page - 1) * per_page,
+                relations: ['etherapy'],
+            });
+
+            return etherapies;
+        } catch (err) {
+            throw new Error('Load all fieldJournals per moderator error');
         }
     }
 }
