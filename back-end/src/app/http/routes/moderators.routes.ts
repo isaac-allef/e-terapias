f0d0acb743d81e64357fa0bef5d6729c69ff01c5
Isaac Allef <isaac_allef@hotmail.com>
2021-05-08 18:34:30 -0300

    feat: add load me moderator route

--- back-end/src/app/http/routes/moderators.routes.ts
+++ back-end/src/app/http/routes/moderators.routes.ts
@@ -2,10 +2,18 @@ import { Router } from 'express';
 import adapterRouter from '../adapters/expressRouter';
 import makeCreateModeratorsContoller from '../../factories/controllers/userManager/makeCreateModeratorsContoller';
 import makeLoadModeratorContoller from '../../factories/controllers/shared/makeLoadModeratorContoller';
+import makeLoadMeModeratorContoller from '../../factories/controllers/userModerator/makeLoadMeModeratorContoller';
+import adapterMiddleware from '../adapters/expressMiddleware';
+import { makeAuthModeratorMiddleware } from '../../factories/middlewares/makeAuthModeratorMiddleware';
 
 const moderatorsRouter = Router();
 
 moderatorsRouter.post('/', adapterRouter(makeCreateModeratorsContoller()));
+moderatorsRouter.get(
+    '/me',
+    adapterMiddleware(makeAuthModeratorMiddleware()),
+    adapterRouter(makeLoadMeModeratorContoller()),
+);
 moderatorsRouter.get('/:id', adapterRouter(makeLoadModeratorContoller()));
 
 export default moderatorsRouter;
