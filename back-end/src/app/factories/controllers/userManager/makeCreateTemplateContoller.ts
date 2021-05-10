7d6eb813abb341519aee4aecd8934d7dde7d6229
Isaac Allef <isaac_allef@hotmail.com>
2021-05-10 15:29:13 -0300

    feat: create template and link with etherapies together

--- back-end/src/app/factories/controllers/userManager/makeCreateTemplateContoller.ts
+++ back-end/src/app/factories/controllers/userManager/makeCreateTemplateContoller.ts
@@ -1,12 +1,15 @@
 import CreateTemplateService from '../../../../core/services/CreateTemplateService';
+import EtherapyTypeormRepository from '../../../../infra/db/typeorm/repositories/EtherapyTypeormRepository';
 import TemplateTypeormRepository from '../../../../infra/db/typeorm/repositories/TemplateTypeormRepository';
 import { CreateTemplateController } from '../../../../presentation/controllers/CreateTemplateController';
 import { Controller } from '../../../../presentation/protocols/controller';
 
 const makeCreateTemplateContoller = (): Controller => {
     const createTemplateRepository = new TemplateTypeormRepository();
+    const linkTemplateToEtherapiesRepository = new EtherapyTypeormRepository();
     const createTemplateService = new CreateTemplateService(
         createTemplateRepository,
+        linkTemplateToEtherapiesRepository,
     );
     return new CreateTemplateController(createTemplateService);
 };
--- back-end/src/core/services/CreateTemplateService.ts
+++ back-end/src/core/services/CreateTemplateService.ts
@@ -1,19 +1,33 @@
 import Template, { templateField } from '../entities/Template';
 import CreateTemplateRepository from '../protocols/db/repositories/CreateTemplateRepository';
+import LinkTemplateToEtherapiesRepository from '../protocols/db/repositories/LinkTemplateToEtherapiesRepository';
 
 export type params = {
     name: string;
     templateFields: templateField[];
+    etherapiesIds: string[];
 };
 
 class CreateTemplateService {
-    constructor(private createTemplateRepository: CreateTemplateRepository) {}
+    constructor(
+        private createTemplateRepository: CreateTemplateRepository,
+        private linkTemplateToEtherapiesRepository: LinkTemplateToEtherapiesRepository,
+    ) {}
 
-    public async execute({ name, templateFields }: params): Promise<Template> {
+    public async execute({
+        name,
+        templateFields,
+        etherapiesIds,
+    }: params): Promise<Template> {
         const template = await this.createTemplateRepository.create({
             name,
             templateFields,
         });
+
+        await this.linkTemplateToEtherapiesRepository.linkTemplate(
+            template,
+            etherapiesIds,
+        );
 
         return template;
     }
