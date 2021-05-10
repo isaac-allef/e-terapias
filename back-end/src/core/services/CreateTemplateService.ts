386a65a646e4bce16f5d072dad74d7b22b007ee6
Isaac Allef <isaac_allef@hotmail.com>
2021-05-10 19:11:37 -0300

    feat: validate template fields

--- back-end/src/core/services/CreateTemplateService.ts
+++ back-end/src/core/services/CreateTemplateService.ts
@@ -1,4 +1,5 @@
 import Template, { templateField } from '../entities/Template';
+import AppError from '../errors/AppError';
 import CreateTemplateRepository from '../protocols/db/repositories/CreateTemplateRepository';
 import LinkTemplateToEtherapiesRepository from '../protocols/db/repositories/LinkTemplateToEtherapiesRepository';
 
@@ -19,6 +20,8 @@ class CreateTemplateService {
         templateFields,
         etherapiesIds,
     }: params): Promise<Template> {
+        this.validateTemplateFields(templateFields);
+
         const template = await this.createTemplateRepository.create({
             name,
             templateFields,
@@ -30,6 +33,31 @@ class CreateTemplateService {
         );
 
         return template;
+    }
+
+    private validateTemplateFields(templateFields: templateField[]) {
+        if (!Object.keys(templateFields).length) {
+            throw new AppError('TemplateFields void.');
+        }
+
+        templateFields.forEach(fieldTemplate => {
+            const LENGTH_FIELD = 2;
+            const lengthField = Object.keys(fieldTemplate).length;
+
+            if (!(lengthField === LENGTH_FIELD)) {
+                throw new AppError(
+                    'Each object in fieldTemplates must have exactly two properties only, name and type',
+                );
+            }
+
+            if (!('name' in fieldTemplate)) {
+                throw new AppError('Property name not found.');
+            }
+
+            if (!('type' in fieldTemplate)) {
+                throw new AppError('Property type not found.');
+            }
+        });
     }
 }
 
