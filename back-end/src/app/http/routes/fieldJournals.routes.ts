6ec33763aad085d8efb6c0da03cca78154923335
Isaac Allef <isaac_allef@hotmail.com>
2021-05-11 20:34:18 -0300

    feat: add update field journal route

--- back-end/src/app/http/routes/fieldJournals.routes.ts
+++ back-end/src/app/http/routes/fieldJournals.routes.ts
@@ -4,6 +4,7 @@ import makeCreateFieldJournalContoller from '../../factories/controllers/userMod
 import makeLoadFieldJournalContoller from '../../factories/controllers/shared/makeLoadFieldJournalContoller';
 import adapterMiddleware from '../adapters/expressMiddleware';
 import { makeAuthModeratorMiddleware } from '../../factories/middlewares/makeAuthModeratorMiddleware';
+import makeUpdateFieldJournalContoller from '../../factories/controllers/userModerator/makeUpdateFieldJournalContoller';
 
 const fieldJournalsRouter = Router();
 
@@ -13,5 +14,10 @@ fieldJournalsRouter.post(
     adapterRouter(makeCreateFieldJournalContoller()),
 );
 fieldJournalsRouter.get('/:id', adapterRouter(makeLoadFieldJournalContoller()));
+fieldJournalsRouter.put(
+    '/:id',
+    adapterMiddleware(makeAuthModeratorMiddleware()),
+    adapterRouter(makeUpdateFieldJournalContoller()),
+);
 
 export default fieldJournalsRouter;
