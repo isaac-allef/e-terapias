cce92f159e125c259d0474eddc6510fc3d19dce5
Isaac Allef <isaac_allef@hotmail.com>
2021-05-11 16:43:00 -0300

    feat: add update template route

--- back-end/src/app/http/routes/templates.routes.ts
+++ back-end/src/app/http/routes/templates.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import adapterRouter from '../adapters/expressRouter';
 import makeCreateTemplateContoller from '../../factories/controllers/userManager/makeCreateTemplateContoller';
 import makeLoadTemplateContoller from '../../factories/controllers/shared/makeLoadTemplateContoller';
+import makeUpdateTemplateContoller from '../../factories/controllers/userManager/makeUpdateTemplateContoller';
 
 const templatesRouter = Router();
 
 templatesRouter.post('/', adapterRouter(makeCreateTemplateContoller()));
 templatesRouter.get('/:id', adapterRouter(makeLoadTemplateContoller()));
+templatesRouter.put('/:id', adapterRouter(makeUpdateTemplateContoller()));
 
 export default templatesRouter;
