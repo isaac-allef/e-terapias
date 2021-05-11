b8f4075b126a8da096cfacc88b992b279d6c4865
Isaac Allef <isaac_allef@hotmail.com>
2021-05-11 18:05:01 -0300

    feat: create LoadAllFieldJournalsPerEtherapyRepository

---
+++ back-end/src/core/protocols/db/repositories/LoadAllFieldJournalsPerEtherapyRepository.ts
@@ -0,0 +1,19 @@
+import FieldJournal from '../../../entities/FieldJournal';
+
+export type params = {
+    etherapyId: string;
+    sort: 'name' | 'created_at' | 'updated_at';
+    direction: 'asc' | 'desc';
+    per_page: number;
+    page: number;
+};
+
+export default interface LoadAllFieldJournalsPerEtherapyRepository {
+    loadAllPerEtherapy({
+        etherapyId,
+        sort,
+        direction,
+        per_page,
+        page,
+    }: params): Promise<FieldJournal[]>;
+}
