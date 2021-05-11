6d5cad892719926485a7222457b9ff56a0d6b6a1
Isaac Allef <isaac_allef@hotmail.com>
2021-05-11 18:06:37 -0300

    feat: create LoadAllFieldJournalsPerEtherapyController

---
+++ back-end/src/presentation/controllers/LoadAllFieldJournalsPerEtherapyController.ts
@@ -0,0 +1,36 @@
+/* eslint-disable import/prefer-default-export */
+import LoadAllFieldJournalsPerEtherapyService from '../../core/services/LoadAllFieldJournalsPerEtherapyService';
+import { MissingParamError } from '../erros/missingParamError';
+import { badRequest, ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class LoadAllFieldJournalsPerEtherapyController implements Controller {
+    constructor(
+        private readonly loadAllFieldJournalsPerEtherapyService: LoadAllFieldJournalsPerEtherapyService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { sort, direction, per_page, page } = httpRequest.query;
+            const { id } = httpRequest.params;
+
+            if (!id) {
+                return badRequest(new MissingParamError('EtherapyId'));
+            }
+
+            const fieldJournal = await this.loadAllFieldJournalsPerEtherapyService.execute(
+                {
+                    etherapyId: id,
+                    sort,
+                    direction,
+                    per_page,
+                    page,
+                },
+            );
+            return ok(fieldJournal);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
