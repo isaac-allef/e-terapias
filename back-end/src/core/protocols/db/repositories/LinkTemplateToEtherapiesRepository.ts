580fbfeaa381089de134494d9e4805011e51a207
Isaac Allef <isaac_allef@hotmail.com>
2021-05-11 16:41:16 -0300

    feat: add unlink clean

--- back-end/src/core/protocols/db/repositories/LinkTemplateToEtherapiesRepository.ts
+++ back-end/src/core/protocols/db/repositories/LinkTemplateToEtherapiesRepository.ts
@@ -1,5 +1,9 @@
 import Template from '../../../entities/Template';
 
 export default interface LinkTemplateToEtherapiesRepository {
-    linkTemplate(template: Template, etherapiesIds: string[]): Promise<boolean>;
+    linkTemplate(
+        template: Template,
+        etherapiesIds: string[],
+        unlinkClean?: boolean,
+    ): Promise<boolean>;
 }
