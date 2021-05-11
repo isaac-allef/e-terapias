895cd49fdb738be747e3d86d6dd9ac54dc4a3c58
Isaac Allef <isaac_allef@hotmail.com>
2021-05-11 20:33:50 -0300

    feat: create UpdateFieldJournalController

---
+++ back-end/src/presentation/controllers/UpdateFieldJournalController.ts
@@ -0,0 +1,46 @@
+/* eslint-disable import/prefer-default-export */
+import UpdateFieldJournalService from '../../core/services/UpdateFieldJournalService';
+import { MissingParamError } from '../erros/missingParamError';
+import { badRequest, ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class UpdateFieldJournalController implements Controller {
+    constructor(
+        private readonly updateFieldJournalService: UpdateFieldJournalService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { name, fields } = httpRequest.body;
+            const { id } = httpRequest.params;
+            const moderatorId = httpRequest.userId;
+
+            if (!id) {
+                return badRequest(new MissingParamError('id'));
+            }
+
+            if (!name) {
+                return badRequest(new MissingParamError('name'));
+            }
+
+            if (!fields) {
+                return badRequest(new MissingParamError('fields'));
+            }
+
+            if (!moderatorId) {
+                return badRequest(new MissingParamError('moderatorId'));
+            }
+
+            const FieldJournal = await this.updateFieldJournalService.execute({
+                id,
+                name,
+                fields,
+                moderatorId,
+            });
+            return ok(FieldJournal);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
