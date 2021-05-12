784cbdaacc149e738b5d370ce38bf28fcdd0f384
Isaac Allef <isaac_allef@hotmail.com>
2021-05-12 12:51:19 -0300

    feat: create LoadAllTemplatesRepository

---
+++ back-end/src/core/protocols/db/repositories/LoadAllTemplatesRepository.ts
@@ -0,0 +1,12 @@
+import Template from '../../../entities/Template';
+
+export type params = {
+    sort: 'name' | 'created_at' | 'updated_at';
+    direction: 'asc' | 'desc';
+    per_page: number;
+    page: number;
+};
+
+export default interface LoadAllTemplatesRepository {
+    loadAll({ sort, direction, per_page, page }: params): Promise<Template[]>;
+}
