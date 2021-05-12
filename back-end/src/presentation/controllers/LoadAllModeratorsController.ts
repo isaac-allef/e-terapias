73e827dbc6784f2522e9ef9981acf536986e03d7
Isaac Allef <isaac_allef@hotmail.com>
2021-05-12 11:34:35 -0300

    feat: create LoadAllModeratorsController

---
+++ back-end/src/presentation/controllers/LoadAllModeratorsController.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+import LoadAllModeratorsService from '../../core/services/LoadAllModeratorsService';
+import { ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class LoadAllModeratorsController implements Controller {
+    constructor(
+        private readonly loadAllModeratorsService: LoadAllModeratorsService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { sort, direction, per_page, page } = httpRequest.query;
+
+            const moderator = await this.loadAllModeratorsService.execute({
+                sort,
+                direction,
+                per_page,
+                page,
+            });
+            return ok(moderator);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
