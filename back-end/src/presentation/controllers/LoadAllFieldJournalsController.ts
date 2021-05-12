1a419d169b8a2c4d677f53809af0bee7e46c9dfd
Isaac Allef <isaac_allef@hotmail.com>
2021-05-12 12:36:20 -0300

    feat: create LoadAllFieldJournalsController

---
+++ back-end/src/presentation/controllers/LoadAllFieldJournalsController.ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/prefer-default-export */
+import LoadAllFieldJournalsService from '../../core/services/LoadAllFieldJournalsService';
+import { ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class LoadAllFieldJournalsController implements Controller {
+    constructor(
+        private readonly loadAllFieldJournalsService: LoadAllFieldJournalsService,
+    ) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { sort, direction, per_page, page } = httpRequest.query;
+
+            const fieldJournal = await this.loadAllFieldJournalsService.execute(
+                {
+                    sort,
+                    direction,
+                    per_page,
+                    page,
+                },
+            );
+            return ok(fieldJournal);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
