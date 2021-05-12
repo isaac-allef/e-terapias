453f05f06f60eaf39bb834b8aad125cf862af698
Isaac Allef <isaac_allef@hotmail.com>
2021-05-12 11:33:54 -0300

    feat: create LoadAllModeratorsRepository

---
+++ back-end/src/core/protocols/db/repositories/LoadAllModeratorsRepository.ts
@@ -0,0 +1,12 @@
+import Moderator from '../../../entities/Moderator';
+
+export type params = {
+    sort: 'name' | 'created_at' | 'updated_at';
+    direction: 'asc' | 'desc';
+    per_page: number;
+    page: number;
+};
+
+export default interface LoadAllModeratorsRepository {
+    loadAll({ sort, direction, per_page, page }: params): Promise<Moderator[]>;
+}
