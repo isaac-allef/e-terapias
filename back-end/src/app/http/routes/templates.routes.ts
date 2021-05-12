674fd73c2fb4ac3041b397643aba5cc6a26cd3e3
Isaac Allef <isaac_allef@hotmail.com>
2021-05-12 12:52:17 -0300

    feat: add load all templates route

--- back-end/src/app/http/routes/templates.routes.ts
+++ back-end/src/app/http/routes/templates.routes.ts
@@ -3,10 +3,12 @@ import adapterRouter from '../adapters/expressRouter';
 import makeCreateTemplateContoller from '../../factories/controllers/userManager/makeCreateTemplateContoller';
 import makeLoadTemplateContoller from '../../factories/controllers/shared/makeLoadTemplateContoller';
 import makeUpdateTemplateContoller from '../../factories/controllers/userManager/makeUpdateTemplateContoller';
+import makeLoadAllTemplatesContoller from '../../factories/controllers/userManager/makeLoadAllTemplatesContoller';
 
 const templatesRouter = Router();
 
 templatesRouter.post('/', adapterRouter(makeCreateTemplateContoller()));
+templatesRouter.get('/', adapterRouter(makeLoadAllTemplatesContoller()));
 templatesRouter.get('/:id', adapterRouter(makeLoadTemplateContoller()));
 templatesRouter.put('/:id', adapterRouter(makeUpdateTemplateContoller()));
 
