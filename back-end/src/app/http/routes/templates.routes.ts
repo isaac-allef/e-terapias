d415e863c7fcbc88bf05993867486058d2d22caf
Isaac Allef <isaac_allef@hotmail.com>
2021-05-12 17:36:11 -0300

    feat: add search templates route

--- back-end/src/app/http/routes/templates.routes.ts
+++ back-end/src/app/http/routes/templates.routes.ts
@@ -4,6 +4,7 @@ import makeCreateTemplateContoller from '../../factories/controllers/userManager
 import makeLoadTemplateContoller from '../../factories/controllers/shared/makeLoadTemplateContoller';
 import makeUpdateTemplateContoller from '../../factories/controllers/userManager/makeUpdateTemplateContoller';
 import makeLoadAllTemplatesContoller from '../../factories/controllers/userManager/makeLoadAllTemplatesContoller';
+import makeSearchTemplatesContoller from '../../factories/controllers/userManager/makeSearchTemplatesContoller';
 
 const templatesRouter = Router();
 
@@ -11,5 +12,9 @@ templatesRouter.post('/', adapterRouter(makeCreateTemplateContoller()));
 templatesRouter.get('/', adapterRouter(makeLoadAllTemplatesContoller()));
 templatesRouter.get('/:id', adapterRouter(makeLoadTemplateContoller()));
 templatesRouter.put('/:id', adapterRouter(makeUpdateTemplateContoller()));
+templatesRouter.get(
+    '/search/:keyword',
+    adapterRouter(makeSearchTemplatesContoller()),
+);
 
 export default templatesRouter;
