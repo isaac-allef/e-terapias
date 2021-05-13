d6012c3e55f6bbcbebbe8641e19dcebb94e26111
Isaac Allef <isaac_allef@hotmail.com>
2021-05-13 20:51:34 -0300

    feat: implement LoadUserByEmailRepository and UpdateAccessTokenRepository to manager user

--- back-end/src/infra/db/typeorm/repositories/ManagerTypeormRepository.ts
+++ back-end/src/infra/db/typeorm/repositories/ManagerTypeormRepository.ts
@@ -5,9 +5,16 @@ import UploadManagersListRepository, {
     params,
 } from '../../../../core/protocols/db/repositories/UploadManagersListRepository';
 import Manager from '../../../../core/entities/Manager';
+import LoadUserByEmailRepository from '../../../../core/protocols/db/repositories/LoadUserByEmailRepository';
+import User from '../../../../core/entities/User';
+import UpdateAccessTokenRepository from '../../../../core/protocols/db/repositories/UpdateAccessTokenRepository';
 
 @EntityRepository()
-class ManagerTypeormRepository implements UploadManagersListRepository {
+class ManagerTypeormRepository
+    implements
+        UploadManagersListRepository,
+        LoadUserByEmailRepository,
+        UpdateAccessTokenRepository {
     private ormRepository: Repository<ManagerTypeorm>;
 
     constructor() {
@@ -42,6 +49,42 @@ class ManagerTypeormRepository implements UploadManagersListRepository {
             return managers;
         } catch {
             throw new Error('Create managers error');
+        }
+    }
+
+    async loadByEmail(email: string): Promise<User> {
+        try {
+            const user = await this.ormRepository.findOne({
+                where: { email },
+            });
+
+            if (!user) {
+                throw new Error('User not found');
+            }
+
+            return user;
+        } catch {
+            throw new Error('Load user error');
+        }
+    }
+
+    async updateAccessToken(id: string, token: string): Promise<User> {
+        try {
+            const user = await this.ormRepository.findOne({
+                where: { id },
+            });
+
+            if (!user) {
+                throw new Error('User not found');
+            }
+
+            user.token = token;
+
+            await this.ormRepository.save(user);
+
+            return user;
+        } catch {
+            throw new Error('Load user error');
         }
     }
 }
