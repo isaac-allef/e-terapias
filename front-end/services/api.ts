8fe2122fb837042f583a33e1e70f323c942645e0
Isaac Allef <isaac_allef@hotmail.com>
2021-05-25 16:03:40 -0300

    feat: export cancelRequest function

--- front-end/services/api.ts
+++ front-end/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const cancelToken = axios.CancelToken;
+const source = cancelToken.source();
+export const cancelRequest = () => source.cancel('Request cancelled')
+
 export const api = axios.create({
     baseURL: 'http://localhost:3333/',
 });
