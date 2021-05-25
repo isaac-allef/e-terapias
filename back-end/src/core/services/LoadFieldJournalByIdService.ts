4165872af12680b9c80e525319df5cedd864e167
Isaac Allef <isaac_allef@hotmail.com>
2021-05-25 18:00:32 -0300

    feat: option filter per moderator id in LoadFieldJournalByIdServie

--- back-end/src/core/services/LoadFieldJournalByIdService.ts
+++ back-end/src/core/services/LoadFieldJournalByIdService.ts
@@ -6,8 +6,20 @@ class LoadFieldJournalByIdService {
         private loadFieldJournalByIdRepository: LoadFieldJournalByIdRepository,
     ) {}
 
-    public async execute(id: string): Promise<FieldJournal> {
-        return this.loadFieldJournalByIdRepository.load(id);
+    public async execute(
+        id: string,
+        verifyModeratorId: string | null = null,
+    ): Promise<FieldJournal> {
+        const fieldJournal = await this.loadFieldJournalByIdRepository.load(id);
+
+        if (!verifyModeratorId) {
+            return fieldJournal;
+        }
+
+        if (verifyModeratorId === fieldJournal.moderator.id) {
+            return fieldJournal;
+        }
+        throw new Error("Moderator id doesn't match");
     }
 }
 
