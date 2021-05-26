a6abff76496d3f5950707b54d28fd92d7a63c9c9
Isaac Allef <isaac_allef@hotmail.com>
2021-05-26 20:04:25 -0300

    refactor: move logic from get sheet to a file outside  of serverless functions

--- front-end/pages/api/readSheet.ts
+++ front-end/pages/api/readSheet.ts
@@ -1,43 +1,14 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSheet } from '../../services/getSheet';
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
     const { docId, docIndex } = request.query;
 
-    const doc = new GoogleSpreadsheet(docId);
+    if (!docId) {
+        return response.status(400).json({error: 'Doc id required'});
+    }
 
-    await doc.useServiceAccountAuth({
-        client_email: process.env.CLIENT_EMAIL,
-        private_key: process.env.PRIVATE_KEY,
-    });
+    const sheet = await getSheet(docId, docIndex);
 
-    await doc.loadInfo();
-
-    const sheet = doc.sheetsByIndex[docIndex || 0];
-
-    await sheet.loadHeaderRow()
-    const columnsNames = sheet.headerValues;
-
-    const rows = await sheet.getRows();
-
-    const stringJson = JSON.stringify(rows, removerMetaDatasAndCircularStructure());
-
-    const objectJson = JSON.parse(stringJson);
-
-    return response.json({columnsNames, objectJson});
+    return response.json(sheet);
 }
-
-const removerMetaDatasAndCircularStructure = () => {
-    const visited = new WeakSet();
-    return (key: any, value: any) => {
-        if (key == "_sheet" || key == "_rowNumber" || key == "_rawData") {
-            return undefined
-        }
-        if (typeof value === "object" && value !== null) {
-            if (visited.has(value)) return;
-            visited.add(value);
-        }
-        return value;
-    };
-};
---
+++ front-end/services/getSheet.ts
@@ -0,0 +1,40 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+export const getSheet = async (docId, docIndex) => {
+
+    const doc = new GoogleSpreadsheet(docId);
+
+    await doc.useServiceAccountAuth({
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY,
+    });
+
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsByIndex[docIndex || 0];
+
+    await sheet.loadHeaderRow()
+    const columnsNames = sheet.headerValues;
+
+    const rows = await sheet.getRows();
+
+    const stringJson = JSON.stringify(rows, removerMetaDatasAndCircularStructure());
+
+    const objectJson = JSON.parse(stringJson);
+
+    return {columnsNames, objectJson};
+}
+
+const removerMetaDatasAndCircularStructure = () => {
+    const visited = new WeakSet();
+    return (key: any, value: any) => {
+        if (key == "_sheet" || key == "_rowNumber" || key == "_rawData") {
+            return undefined
+        }
+        if (typeof value === "object" && value !== null) {
+            if (visited.has(value)) return;
+            visited.add(value);
+        }
+        return value;
+    };
+};
