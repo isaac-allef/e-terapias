af3bcfacbfcb6a326010992f811076edcaac193f
Isaac Allef <isaac_allef@hotmail.com>
2021-06-02 17:25:53 -0300

    feat: create timestampFormat functions in util layer

---
+++ front-end/utils/timestampFormat.ts
@@ -0,0 +1,12 @@
+export const timestampToDateTime = (timestamp: string): string => {
+    return new Date(timestamp).toLocaleString('pt-br');
+};
+
+export const timestampToDate = (timestamp: string): string => {
+    return new Date(timestamp).toLocaleString('pt-br', {
+        weekday: 'long',
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+    });
+};
