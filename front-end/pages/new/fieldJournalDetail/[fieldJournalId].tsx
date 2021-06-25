a5b633a8d03bdcb7eed6e526f391526b6443dbc3
Isaac Allef <isaac_allef@hotmail.com>
2021-06-25 10:31:33 -0300

    feat: field journal details, view different question types

--- front-end/pages/new/fieldJournalDetail/[fieldJournalId].tsx
+++ front-end/pages/new/fieldJournalDetail/[fieldJournalId].tsx
@@ -9,6 +9,7 @@ import MyMenu from "../../../components/new/MyMenu";
 import Link from "next/link";
 import MyDivider from "../../../components/shared/MyDivider";
 import MyButton from "../../../components/shared/MyButton";
+import { Checkbox, CheckboxGroup, Input, Radio, RadioGroup, Textarea } from "@chakra-ui/react";
 
 export default function FieldJournalDetail() {
     const router = useRouter();
@@ -75,18 +76,74 @@ const fields = (fields) =>  (
 		{React.Children.toArray(fields.map(field => {
 			return <>
 			<Text>{field.name}</Text>
-			<Box 
-				borderWidth="1px" 
-				rounded='md'
-				paddingTop='0.5rem'
-				paddingBottom='0.5rem'
-				paddingLeft='1rem'
-				paddingRight='1rem'
-			>{field.value}</Box>
+			{showField(field)}
 			</>
 		}))}
 	</Box>
 )
+
+const showField = (field) => {
+	if (['short', 'long', 'date', 'linear', 'choice', 'check'].includes(field.type)) {
+
+		if (field.type === 'short' || field.type === 'date') {
+			return <Input isDisabled value={field.value}/>
+		}
+	
+		if (field.type === 'long') {
+			return <Textarea isDisabled value={field.value}/>
+		}
+
+		if (field.type === 'linear') {
+			const arrayOptions = Array.from(
+				{ length: parseInt(field.options[1]) }, 
+				(_, i) => (i + parseInt(field.options[0]) + '')
+			)
+			return <>
+				<RadioGroup value={field.value}>
+				{
+					React.Children.toArray(arrayOptions.map(option => {
+						return <Radio isDisabled value={option}>{ option }</Radio>
+					}))
+				}
+				</RadioGroup>
+			</>
+		}
+
+		if (field.type === 'choice') {
+			return <>
+				<RadioGroup value={field.value}>
+				{
+					React.Children.toArray(field.options.map(option => {
+						return <Radio isDisabled value={option}>{ option }</Radio>
+					}))
+				}
+				</RadioGroup>
+			</>
+		}
+
+		if (field.type === 'check') {
+			return <>
+				<CheckboxGroup value={field.value}>
+				{
+					React.Children.toArray(field.options.map(option => {
+						return <Checkbox isDisabled value={option}>{ option }</Checkbox>
+					}))
+				}
+				</CheckboxGroup>
+			</>
+		}
+	}
+	else {
+		return <Box 
+			borderWidth="1px" 
+			rounded='md'
+			paddingTop='0.5rem'
+			paddingBottom='0.5rem'
+			paddingLeft='1rem'
+			paddingRight='1rem'
+		>{field.value}</Box>
+	}
+}
 
 const getFieldJournal = async (token: string, id: string): Promise<any> => {
 	const response = await api.get(`/fieldJournals/${id}`, {
