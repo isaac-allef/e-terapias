eed93565dfaf07bd53bf726b27792eb77ce317fd
Isaac Allef <isaac_allef@hotmail.com>
2021-06-26 19:15:17 -0300

    feat: create offer entity

---
+++ back-end/src/core/entities/Offer.ts
@@ -0,0 +1,41 @@
+// import Manager from './Manager';
+
+type settings_service_account = {
+    client_email: string;
+    private_key: string;
+};
+
+type settings_moderators = {
+    sheet_link: string;
+    column_email: string;
+    column_name: string;
+    column_etherapies_identifiers: string;
+};
+
+type settings_etherapies = {
+    sheet_link: string;
+    column_identifier: string;
+    column_name: string;
+};
+
+export type settings = {
+    serviceAccount: settings_service_account;
+    moderators: settings_moderators;
+    etherapies: settings_etherapies;
+};
+
+interface Offer {
+    id: string;
+
+    name: string;
+
+    dateStart: Date;
+
+    dateEnd: Date;
+
+    settings: settings;
+
+    // supervisor: Manager;
+}
+
+export default Offer;
