4b6683ac1c15eaff03afbf1dd662c6afc3df9f36
Isaac Allef <isaac_allef@hotmail.com>
2021-06-26 19:18:32 -0300

    feat: create CreateOfferController

---
+++ back-end/src/presentation/controllers/CreateOfferController.ts
@@ -0,0 +1,47 @@
+/* eslint-disable import/prefer-default-export */
+import CreateOfferService from '../../core/services/CreateOfferService';
+import { MissingParamError } from '../erros/missingParamError';
+import { badRequest, ok, serverError } from '../helpers/httpHelder';
+import { Controller } from '../protocols/controller';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
+export class CreateOfferController implements Controller {
+    constructor(private readonly createOfferService: CreateOfferService) {}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { name, dateStart, dateEnd, settings } = httpRequest.body;
+            // const managerId = httpRequest.userId;
+
+            if (!name) {
+                return badRequest(new MissingParamError('name'));
+            }
+
+            if (!dateStart) {
+                return badRequest(new MissingParamError('dateStart'));
+            }
+
+            if (!dateEnd) {
+                return badRequest(new MissingParamError('dateEnd'));
+            }
+
+            if (!settings) {
+                return badRequest(new MissingParamError('settings'));
+            }
+
+            // if (!managerId) {
+            //     return badRequest(new MissingParamError('managerId'));
+            // }
+
+            const offer = await this.createOfferService.execute({
+                name,
+                dateStart,
+                dateEnd,
+                settings,
+            });
+            return ok(offer);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
