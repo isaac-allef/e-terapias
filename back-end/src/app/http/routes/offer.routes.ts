0c8dab611bf3928c4ff8bab207b6ce2a96645914
Isaac Allef <isaac_allef@hotmail.com>
2021-06-27 16:47:45 -0300

    feat: add update offer route

--- back-end/src/app/http/routes/offer.routes.ts
+++ back-end/src/app/http/routes/offer.routes.ts
@@ -1,6 +1,7 @@
 import { celebrate, errors, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
 import makeCreateOfferController from '../../factories/controllers/userManager/makeCreateOfferController';
+import makeUpdateOfferController from '../../factories/controllers/userManager/makeUpdateOfferController';
 import adapterRouter from '../adapters/expressRouter';
 import { authManager } from '../middlewares/authManager';
 
@@ -34,6 +35,39 @@ offerRouter.post(
     }),
     authManager,
     adapterRouter(makeCreateOfferController()),
+);
+
+offerRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: Joi.object().keys({
+            id: Joi.string().required(),
+        }),
+        [Segments.BODY]: Joi.object().keys({
+            name: Joi.string().required(),
+            dateStart: Joi.date().required(),
+            dateEnd: Joi.date().required(),
+            settings: Joi.object().keys({
+                serviceAccount: Joi.object().keys({
+                    client_email: Joi.string(),
+                    private_key: Joi.string(),
+                }),
+                moderators: Joi.object().keys({
+                    sheet_link: Joi.string(),
+                    column_email: Joi.string(),
+                    column_name: Joi.string(),
+                    column_etherapies_identifiers: Joi.string(),
+                }),
+                etherapies: Joi.object().keys({
+                    sheet_link: Joi.string(),
+                    column_identifier: Joi.string(),
+                    column_name: Joi.string(),
+                }),
+            }),
+        }),
+    }),
+    authManager,
+    adapterRouter(makeUpdateOfferController()),
 );
 
 offerRouter.use(errors());
